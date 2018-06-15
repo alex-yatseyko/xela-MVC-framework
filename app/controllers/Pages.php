67fa7aca3ac0ac5f16c6bcb8f17f85671021df68
Alex <alex.yatseiko@gmail.com>
2018-06-15 15:25:02 +0300

    fouteenth

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -7,8 +7,13 @@
 
 		public function index(){
 			// $this->view('hello'); // Testig the view if it exists or not. The index method is default method. 
-			$data = ['title' => '
-			Welcome'];
+
+			$posts = $this->postModel->getPosts();
+
+			$data = [
+				'title' => 'Welcome',
+				'posts' => $posts
+			];
 
 			$this->view('pages/index', $data);
 		}
